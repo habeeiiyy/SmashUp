97fd4b0e5c3d867ed60d09240f66d46da4bff221
habeeiiyy <habeebdeveloper.india@gmail.com>
2026-08-01 17:19:42 +0000

    custom register with email added

---
+++ backend/accounts/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+class RegisterationForm(UserCreationForm):
+    email=forms.EmailField(required=False)
+    class Meta:
+        model=User
+        fields=["username","email","password1","password2"]
+        
+    
+
+
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterationForm
 # Create your views here.
 def registration(request):
     if request.method=="POST":
-        user_form=UserCreationForm(request.POST)
+        user_form=RegisterationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
             return HttpResponse("<h1>registration is succesfull!!</h1>")
     else:
-        user_form=UserCreationForm()
+        user_form=RegisterationForm()
     return render(request,"accounts/register.html",{'user_form':user_form})
