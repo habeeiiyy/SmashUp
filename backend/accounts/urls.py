6504fe387ed301161c80334b6a4a73377f57b7a1
habeeiiyy <habeebdeveloper.india@gmail.com>
2026-08-02 14:31:43 +0000

    register,login and logout work perfectly

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.registration,name="register"),
-    path('login',views.login_view,name="login"),
-    path('home',views.home,name='home'),
+    path('login/',views.login_view,name="login"),
+    path('home/',views.home,name='home'),
     path('logout/',views.logout_view,name="logout")
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import RegisterationForm
 from django.contrib import messages
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import login,logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
@@ -10,22 +10,14 @@ from django.utils.http import url_has_allowed_host_and_scheme
 def registration(request):
     if request.method == "POST":
         user_form = RegisterationForm(request.POST)
-
-        print("FORM VALID:", user_form.is_valid())
-        print("FORM ERRORS:", user_form.errors)
-
         if user_form.is_valid():
             user = user_form.save()
             login(request, user)
-            print("LOGIN SUCCESS")
             return redirect("home")
     else:
         user_form = RegisterationForm()
     return render(
-        request,
-        "accounts/register.html",
-        {"user_form": user_form}
-    )
+        request,"accounts/register.html",{"user_form": user_form})
 @never_cache
 def login_view(request):
     if request.user.is_authenticated:
